bd6ee45c162d89ca401a9fc19f209e8cefe721f9
franklin <franklinbenjamin032@gmail.com>
2021-01-08 18:28:12 -0400

    add my_each method

--- enumerables.rb
+++ enumerables.rb
@@ -1,8 +1,12 @@
 module Enumerable
+  # def my_each
+  #   0.upto(length - 1) do |v|
+  #     yield(self[v])
+  #   end
+  # end
 
-  def my_each
-    #code here
-  end
-
-
+  
 end
+
+puts 'my_each' + '----------------------------------'
+[2, 3, 7, 8, 9, 132].my_each { |v| puts "this is sequence#{v}" }
