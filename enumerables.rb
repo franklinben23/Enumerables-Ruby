6bec26a4e29329becb32913628910d20e13ded0c
franklin <franklinbenjamin032@gmail.com>
2021-01-11 12:59:25 -0400

    update my_each and my_each_with_index methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,17 +1,33 @@
 module Enumerable
   def my_each
     h_array = to_a
+    unless block_given? 
+      0.upto(length - 1) do |v|
+      h_array[v]
+      end
 
-    0.upto(length - 1) do |v|
+    else 0.upto(length - 1) do |v|
       yield(h_array[v])
     end
+  end
   end
 
   def my_each_with_index
     h_array = to_a
-    0.upto(length - 1) do |v|
+    unless block_given? 
+      0.upto(length - 1) do |v|
+      puts h_array[v], v
+      end
+
+    else 0.upto(length - 1) do |v|
       yield(h_array[v], v)
     end
+  end
+  end
+
+  def my_select
+    #iterate trough every instance of the array, check for the ones that make the condition true and return them into a new array.
+    self.my_each{}
   end
 end
 
@@ -19,7 +35,7 @@ end
 # { fish: 'shark', bird: 'rooster'}.my_each { |v| puts "this is sequence#{v}" }
 
 # puts 'my_each' + '----------------------------------'
-# [2, 3, 7, 8, 9, 132].my_each { |v, i| puts "this is sequence#{v}" }
+# [2, 3, 7, 8, 9, 132].my_each { |v| puts "this is sequence#{v}" }
 
 puts 'my_each_with_index for hash' + '----------------------------------'
 { fish: 'shark', bird: 'rooster' }.my_each_with_index { |v, i| puts "this is key-pair#{v} and it's index is #{i}" }
